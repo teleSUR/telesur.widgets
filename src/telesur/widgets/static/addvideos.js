bbb2927907f87097c77cf070752981d6dfcb5c19
Franco Pellegrini <frapell@gmail.com>
2012-01-05 00:53:20 -0300

    Commiteado el widget para agregar videos a un NITF andando. Tambien agregado js adicional necesario (y provisorio, hasta decidir como vamos a implementar bien este widget)

---
+++ src/telesur/widgets/static/addvideos.js
@@ -0,0 +1,32 @@
+// Esto es provisorio hasta que veamos una mejor implementacion
+
+function addVideoToNITF(video){
+    title = video.text();
+    url = video.attr('href');
+    jQuery.ajax({type: 'POST',
+                    url: '@@add-video-to-context',
+                    async : false,
+                    data: {'title':title,
+                           'url':url},
+                    success: function(results){
+                        // Necesitamos hacer algo con el resultado ?
+                            }
+                });
+}
+
+if(jQuery) (function($){
+    
+    $.extend($.fn, {
+        contentTreeAddVideos: function() {
+            var contenttree_window = (this).parents(".contenttreeWindow");
+            var input_box = $('#'+ contenttree_window[0].id.replace(/-contenttree-window$/,"-widgets-query"));
+            contenttree_window.find('.navTreeCurrentItem > a').each(function () {
+                addVideoToNITF($(this));
+//                 removeFromDroppable( $(this) );
+            });
+
+            $(this).contentTreeCancel();
+        }
+    });
+    
+})(jQuery);
--- src/telesur/widgets/videos.py
+++ src/telesur/widgets/videos.py
@@ -15,34 +15,97 @@ from collective.formwidget.relationfield.widget \
 from plone.app.layout.navigation.interfaces import INavtreeStrategy
 from plone.app.layout.navigation.navtree import buildFolderTree
 
+from telesur.api.interfaces import IPortalAPI
+from zope.component import queryUtility
+from zope.i18n import translate
 
 class AddVideosWidget(BaseWidget):
     display_template = ViewPageTemplateFile('templates/add_videos_widget.pt')
+    recurse_template = ViewPageTemplateFile('templates/recurse_videos_widget.pt')
 
-    def render_tree(self, relPath=None, query=None, limit=10):
-        # Todo lo de aqui abajo tiene que ser reemplazado por una query
-        # que traiga el listado de videos.
-        content = self.context
-        portal_state = getMultiAdapter((self.context, self.request),
-                                          name=u'plone_portal_state')
-        portal = portal_state.portal()
-        source = self.bound_source
-        if query is not None:
-            source.navigation_tree_query = query
-        strategy = getMultiAdapter((portal, self), INavtreeStrategy)
-        if relPath is not None:
-            root_path = portal_state.navigation_root_path()
-            rel_path = root_path + '/' + relPath
-            strategy.rootPath = rel_path
-        data = buildFolderTree(portal,
-                               obj=portal,
-                               query=source.navigation_tree_query,
-                               strategy=strategy)
+    def render_tree(self, limit=10):
+        data = []
+        url = "http://multimedia.tlsur.net/api/clip/?ultimo=%s"%limit
 
-        return self.recurse_template(
-                                    children=data.get('children', [])[:limit],
-                                    level=1)
-                                    
+        video_api = getMultiAdapter((self.context, self.request), name="video_api")
+
+        json = video_api.get_json(url)
+
+        for entry in json:
+            if entry['archivo_url']:
+                # Este checkeo no debería ser necesario. Todos los videos
+                # deberían tener un URL válido
+                data.append(
+                        {
+                            'video_url' : entry['archivo_url'],
+                            'video_thumb' : entry['thumbnail_mediano'],
+                            'selectable' : True,
+                            'description' : entry['descripcion'],
+                        }
+                    )
+                    
+        return self.recurse_template(children=data,level=1)
+
+
+    def js_extra(self):
+        form_url = self.request.getURL()
+        url = "%s/++widget++%s/@@contenttree-fetch" % (form_url, self.name)
+
+        return """\
+
+                $('#%(id)s-widgets-query').each(function() {
+                    if($(this).siblings('input.searchButton').length > 0) { return; }
+                    $(document.createElement('input'))
+                        .attr({
+                            'type': 'button',
+                            'value': '%(button_val)s'
+                        })
+                        .addClass('searchButton')
+                        .click( function () {
+                            var parent = $(this).parents("*[id$='-autocomplete']")
+                            var window = parent.siblings("*[id$='-contenttree-window']")
+                            window.showDialog();
+                        }).insertAfter($(this));
+                });
+                $('#%(id)s-contenttree-window').find('.contentTreeAddVideos').unbind('click').click(function () {
+                    $(this).contentTreeAddVideos();
+                });
+                $('#%(id)s-contenttree-window').find('.contentTreeCancel').unbind('click').click(function () {
+                    $(this).contentTreeCancel();
+                });
+                $('#%(id)s-widgets-query').after(" ");
+                $('#%(id)s-contenttree').contentTree(
+                    {
+                        script: '%(url)s',
+                        folderEvent: '%(folderEvent)s',
+                        selectEvent: '%(selectEvent)s',
+                        expandSpeed: %(expandSpeed)d,
+                        collapseSpeed: %(collapseSpeed)s,
+                        multiFolder: %(multiFolder)s,
+                        multiSelect: %(multiSelect)s,
+                    },
+                    function(event, selected, data, title) {
+                        // alert(event + ', ' + selected + ', ' + data + ', ' + title);
+                    }
+                );
+        """ % dict(url=url,
+                   id=self.name.replace('.', '-'),
+                   folderEvent=self.folderEvent,
+                   selectEvent=self.selectEvent,
+                   expandSpeed=self.expandSpeed,
+                   collapseSpeed=self.collapseSpeed,
+                   multiFolder=str(self.multiFolder).lower(),
+                   multiSelect=str(self.multi_select).lower(),
+                   name=self.name,
+                   klass=self.klass,
+                   title=self.title,
+                   button_val=translate(
+                       u'heading_contenttree_browse',
+                       default=u'Browse for items',
+                       domain='collective.formwidget.relationfield',
+                       context=self.request))
+    
+
 @implementer(z3c.form.interfaces.IFieldWidget)
 def AddVideosFieldWidget(field, request):
     return z3c.form.widget.FieldWidget(field, AddVideosWidget(request))
