6b5137db9cd70402b427b19fd8c6b83347d9b59e
hvelarde <hector.velarde@gmail.com>
2012-01-06 16:23:12 -0600

    se actualiza la declaración del paquete

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ version = '1.0'
 long_description = (
     open('README.rst').read()
     + '\n' +
-    'Contributors\n'
-    '============\n'
+    open('docs/CREDITS.txt').read()
     + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
+    open('docs/CHANGES.txt').read()
     + '\n')
 
 setup(name='telesur.widgets',
@@ -21,30 +18,32 @@ setup(name='telesur.widgets',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Development Status :: 3 - Alpha",
         "Framework :: Plone :: 4.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
         "Programming Language :: Python",
         ],
       keywords='',
       author='Joaquin Rosales',
       author_email='globojorro@gmail.com',
       url='https://github.com/desarrollotv/telesur.widgets',
-      license='gpl',
+      license='GPL',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
       namespace_packages=['telesur'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'collective.formwidget.relationfield',
-          'collective.nitf',
-      ],
+        'setuptools',
+        'collective.formwidget.relationfield',
+        'collective.nitf',
+        ],
       extras_require={
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
   	  [z3c.autoinclude.plugin]
   	  target = plone
       """,
