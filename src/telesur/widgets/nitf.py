50d8caaf32bc019a6eb18d21f1f8c9c1a64ece43
Franco Pellegrini <frapell@gmail.com>
2012-02-02 13:43:34 -0300

    Agregada fecha y seccion a los nitf relacionados. Tambien agregado un boton para 'mostrar mas' y arreglado el problema de que se redirecciona al hacer click

--- src/telesur/widgets/nitf.py
+++ src/telesur/widgets/nitf.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from zope.component import getMultiAdapter
 from Products.Five.browser import BrowserView
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from zope.interface import implementer
@@ -15,10 +16,28 @@ from Products.CMFCore.utils import getToolByName
 from plone.formwidget.autocomplete.widget import AutocompleteSearch
 from plone.app.vocabularies.catalog import parse_query
 
+from plone.app.layout.navigation.navtree import buildFolderTree
+
+from plone.app.layout.navigation.interfaces import INavtreeStrategy
+from plone.formwidget.contenttree.navtree import NavtreeStrategy
+
+from DateTime import DateTime
+
+class NITFTreeStrategy(NavtreeStrategy):
+    
+    def decoratorFactory(self, node):
+        new_node = super(NITFTreeStrategy, self).decoratorFactory(node)
+        # Pongamos la fecha en un objeto "usable"
+        new_node['creation_date'] = DateTime(new_node['creation_date'])
+        # Y agregamos la sección del NITF
+        new_node['section'] = getattr(node['item'], 'section', "")
+
+        return new_node
+        
 
 class FilterRelatedNitf(BrowserView):
 
-    def __call__(self, query=None):
+    def __call__(self, query=None, offset=0):
         base_query = {'portal_type': 'collective.nitf.content',
                       'sort_on': 'modified',
                       'sort_order': 'reverse',
@@ -29,7 +48,8 @@ class FilterRelatedNitf(BrowserView):
 
         result = self.context.render_tree(relPath='articulos',
                                           query=base_query,
-                                          limit=10)
+                                          limit=10,
+                                          offset=int(offset))
 
         return result.strip()
 
@@ -73,6 +93,35 @@ class ContentRelationWidget(BaseWidget):
     """
     """
     input_template = ViewPageTemplateFile('templates/nitf_related_input.pt')
+    recurse_template = ViewPageTemplateFile('templates/nitf_related_recurse.pt')
+
+    def render_tree(self, relPath=None, query=None, limit=10, offset=0):
+        content = self.context
+        portal_state = getMultiAdapter((self.context, self.request),
+                                          name=u'plone_portal_state')
+        portal = portal_state.portal()
+        source = self.bound_source
+        if query is not None:
+            source.navigation_tree_query = query
+
+        strategy = getMultiAdapter((portal, self),
+                                   INavtreeStrategy,
+                                   name="nitf-tree-strategy")
+
+        if relPath is not None:
+            root_path = portal_state.navigation_root_path()
+            rel_path = root_path + '/' + relPath
+            strategy.rootPath = rel_path
+        
+        data = buildFolderTree(portal,
+                               obj=portal,
+                               query=source.navigation_tree_query,
+                               strategy=strategy)
+
+        return self.recurse_template(
+                        children=data.get('children', [])[offset:offset+limit],
+                        level=1,
+                        offset=offset+limit)
 
     def autocomplete_url(self):
         """
@@ -91,6 +140,55 @@ class ContentRelationWidget(BaseWidget):
         function filterNITF(){
         var query = document.getElementById('form-widgets-search-nitf-related').value;
         $("ul#related-content-nitf").load('%(url)s',{'query':query});
+        }
+        
+        function removeNITFFromDroppable( $item ) {
+            var $dropping = jq('#content-droppable');
+            var $listing = jq('.contenttreeWidget .navTree');
+            var $item_href = $item.find('a.ui-widget-content').attr('href');
+            var $listing_item = $('.contenttreeWidget a@[href='+$item_href+']').parent();
+            $listing_item.removeClass('navTreeCurrentItem');
+            $listing_item.addClass("draggable");
+            $item.remove();
+        }
+
+        function unbindClickEvent() {
+
+            $('ul#content-droppable').unbind('click').click(function(event) {
+                var $item = $(this);
+                var $target = $(event.target);
+                var $parent = $($target).parent();
+
+                if ( $target.is("a.ui-icon-trash") ) {
+                    removeNITFFromDroppable($parent);
+                    return false
+                    }
+                if ( $target.is("a.ui-widget-content") ) {
+                    return false
+                    }
+            });
+
+            $('ul#related-content-nitf > li > a').unbind('click').click(function(event) {
+                return false
+
+            });
+
+        }
+        
+        function appendMoreNitf(offset) {
+            $("#show-more-results").remove();
+            var query = document.getElementById('form-widgets-search-nitf-related').value;
+
+            jQuery.ajax({type: 'POST',
+                        url: '%(url)s',
+                        async : true,
+                        data: {'query':query,
+                                'offset':offset},
+                        success: function(results){
+                                $("ul#related-content-nitf").append(results);
+                                unbindClickEvent();
+                                }
+                            });
         }
         """ % dict(url=url)
 
